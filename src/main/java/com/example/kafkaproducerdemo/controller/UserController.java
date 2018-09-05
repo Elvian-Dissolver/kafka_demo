c073e0f0c7adc1b350105f88da612634c96174cf
Indra Elvian <37201759+Elvian-Dissolver@users.noreply.github.com>
2018-09-05 16:05:10 +0700

    Add files via upload

---
+++ src/main/java/com/example/kafkaproducerdemo/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.example.kafkaproducerdemo.controller;
+
+import com.example.kafkaproducerdemo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("kafka")
+
+public class UserController {
+
+    @Autowired
+    private KafkaTemplate<String, User> kafkaTemplate;
+
+    private static final String TOPIC = "Kafka_Demo";
+
+    @GetMapping("/publish/{name}")
+    public String post(@PathVariable("name") final String name) throws Exception {
+        {
+
+            kafkaTemplate.send(TOPIC, new User(name, "Technology", 12000L));
+
+            return "Published successfully";
+        }
+    }
+}
